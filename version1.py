f32088d576301e846d1f93b422733933fd51c77d
Gan Teck Ann <91054481+teckann@users.noreply.github.com>
2025-03-02 01:25:53 +0800

    Updated

--- version1.py
+++ version1.py
@@ -1,12 +1,10 @@
 import qrcode
-import tkinter as tk
-from tkinter import filedialog, messagebox
 
-link = 'https://music.youtube.com/watch?v=MHCsrKA9gh8&si=zyQKJCV63fsi3xwY'
+link = '<paste the link here>'
 
 qr = qrcode.QRCode(version = 1, box_size = 5, border = 5)
 qr.add_data(link)
 qr.make()
 
 img = qr.make_image(fill_color = 'black', back_color = 'white')
-img.save('link.png')
+img.save('<put the qrcode file name here>.png')
